21a0ff1df28714566b3a529a5993fca7bbadba0a
Grayson Halliday <graysonhalliday@Graysons-MacBook-Pro-6.local>
2021-08-10 11:24:01 -0700

    Re-include deleted files within optimised code merge

---
+++ GH_Gen_Table.c
@@ -0,0 +1,37 @@
+#include "128_Bit_Arithmetic.h"
+
+char mod[ARITHMETIC_STRING_BUFF_LEN] = "16157591";
+
+int main(void) {
+	unsigned char valueBuf[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+	unsigned char counterBuf[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+	unsigned char modBuf[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+	unsigned char resultBuf[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+	unsigned char oneBuf[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+	unsigned char twoBuf[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+
+	stringToBinary("0", valueBuf);
+	stringToBinary("1", oneBuf);
+	stringToBinary("2", twoBuf);
+	stringToBinary(mod, modBuf);
+
+	FILE *fptr = fopen("./top.bin","w+");
+
+	while (lessThanEqual(counterBuf, modBuf) == 1) {
+		// Calculate the square of the index
+		montgomeryMultiplicationHelper(valueBuf, valueBuf, modBuf, resultBuf);
+
+		// Write the result to file
+		fwrite(resultBuf+ARITHMETIC_BINARY_STORE_LEN, sizeof(unsigned char), ARITHMETIC_BINARY_STORE_LEN, fptr);
+
+		// Increment index
+		addition(valueBuf, oneBuf, resultBuf);
+		memcpy(valueBuf, resultBuf, ARITHMETIC_BINARY_BUFF_LEN);
+
+		// Increment counter
+		addition(counterBuf, twoBuf, resultBuf);
+		memcpy(counterBuf, resultBuf, ARITHMETIC_BINARY_BUFF_LEN);
+	}
+
+	fclose(fptr);
+}
---
+++ GH_Modular_Exponentiation.h
@@ -0,0 +1,62 @@
+#include "128_Bit_Arithmetic.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+void mod_encrypt_decrypt(const unsigned char *num, const unsigned char *exp, const unsigned char *mod, unsigned char* out) {
+    memset(out, 0, ARITHMETIC_BINARY_BUFF_LEN);
+
+    unsigned char z0[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+    z0[ARITHMETIC_BINARY_BUFF_LEN-1] |= 1;
+
+    unsigned char z1[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+    z1[ARITHMETIC_BINARY_BUFF_LEN-1] |= 1;
+
+    unsigned char p0[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+    memcpy(p0, num, ARITHMETIC_BINARY_BUFF_LEN);
+
+    unsigned char p1[ARITHMETIC_BINARY_BUFF_LEN] = {0};
+
+    size_t expBits = ARITHMETIC_BINARY_BUFF_LEN*8 - bitOffset(exp);
+    size_t byteInd = ARITHMETIC_BINARY_BUFF_LEN - 1;
+    bool carry = false;
+    unsigned short bitMask = 1;
+    size_t bitCounter = 0;
+
+    // Loop until byteInd is 0
+    while (1) {
+        // Loop through each byte
+        while (bitMask <= 128) {
+            printf("bitCounter is %zu\n", bitCounter);
+            /*
+             * Exit if all non-0 exp bits have been used 
+             */
+            if (bitCounter == expBits) {
+                break;
+            }
+
+            montgomeryMultiplicationHelper(p0, p0, mod, p1);
+            if (exp[byteInd] & bitMask) {
+                montgomeryMultiplicationHelper(z0, p0, mod, z1);
+            } else {
+                memcpy(z1, z0, ARITHMETIC_BINARY_BUFF_LEN);
+            }
+
+            memcpy(p0, p1, ARITHMETIC_BINARY_BUFF_LEN);
+            memcpy(z0, z1, ARITHMETIC_BINARY_BUFF_LEN);
+
+
+            bitMask <<= 1;
+            ++bitCounter;
+        }
+
+        bitMask = 1;
+        if(byteInd == 0 || bitCounter == expBits) {
+            break;
+        } else {
+            --byteInd;
+        }
+    }
+
+    memcpy(out, z1, ARITHMETIC_BINARY_BUFF_LEN);
+}
